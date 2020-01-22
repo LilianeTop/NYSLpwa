3239596b77cdf9555e8f64b28d5ab6d55e69020b
LilianeTop <hallo@lilianetop.nl>
2020-01-22 16:35:33 +0100

    first commit

---
+++ src/components/app.vue
@@ -0,0 +1,170 @@
+<template>
+  <f7-app :params="f7params">
+    <!-- Left panel with cover effect-->
+    <f7-panel left swipe cover class="bg-color-green">
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Menu"></f7-navbar>
+          <f7-list>
+            <f7-list-item link="#" title="Home">
+              <f7-icon slot="media" icon="demo-list-icon"></f7-icon>
+            </f7-list-item>
+            <f7-list-item link="#" title="Game Schedule">
+              <f7-icon slot="media" icon="demo-list-icon"></f7-icon>
+            </f7-list-item>
+            <f7-list-item link="#" title="Teams">
+              <f7-icon slot="media" icon="demo-list-icon"></f7-icon>
+            </f7-list-item>
+            <f7-list-item link="#" title="Players">
+              <f7-icon slot="media" icon="demo-list-icon"></f7-icon>
+            </f7-list-item>
+            <f7-list-item v-if="isSignedIn" link="#" title="Chat">
+              <f7-icon slot="media" icon="demo-list-icon"></f7-icon>
+            </f7-list-item>
+            <f7-list-item link="#" title="Contact">
+              <f7-icon slot="media" icon="demo-list-icon"></f7-icon>
+            </f7-list-item>
+          </f7-list>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
+
+    <!-- Right panel with reveal effect-->
+    <f7-panel right reveal theme-dark>
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Right Panel"></f7-navbar>
+          <f7-block>Right panel content goes here</f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-panel>
+
+    <!-- Your main view, should have "view-main" class -->
+    <f7-view main class="safe-areas" url="/"></f7-view>
+
+    <!-- Popup -->
+    <f7-popup id="my-popup">
+      <f7-view>
+        <f7-page>
+          <f7-navbar title="Popup">
+            <f7-nav-right>
+              <f7-link popup-close>Close</f7-link>
+            </f7-nav-right>
+          </f7-navbar>
+          <f7-block>
+            <p>Popup content goes here.</p>
+          </f7-block>
+        </f7-page>
+      </f7-view>
+    </f7-popup>
+
+    <f7-login-screen id="login-screen">
+      <f7-view>
+        <f7-page login-screen>
+          <f7-login-screen-title>Login</f7-login-screen-title>
+          <f7-list form>
+            <f7-list-input
+              type="text"
+              name="username"
+              placeholder="Your username"
+              :value="username"
+              @input="username = $event.target.value"
+            ></f7-list-input>
+            <f7-list-input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              :value="password"
+              @input="password = $event.target.value"
+            ></f7-list-input>
+          </f7-list>
+          <f7-list>
+            <f7-list-button title="Sign In" login-screen-close @click="login"></f7-list-button>
+            <f7-block-footer>
+              Some text about login information.
+              <br />Click "Sign In" to close Login Screen
+            </f7-block-footer>
+          </f7-list>
+        </f7-page>
+      </f7-view>
+    </f7-login-screen>
+  </f7-app>
+</template>
+<script>
+import routes from "../js/routes.js";
+const firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAibTbTOH_foNgXRdcT8SkJzGGMtx_lWcg",
+  authDomain: "nysl-261007.firebaseapp.com",
+  databaseURL: "https://nysl-261007.firebaseio.com",
+  projectId: "nysl-261007",
+  storageBucket: "nysl-261007.appspot.com",
+  messagingSenderId: "304886205033",
+  appId: "1:304886205033:web:5470ca0a7f9ced6887b0f8",
+  measurementId: "G-E4BPBCGEPZ"
+};
+
+if (!firebase.apps.length) {
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+}
+const auth = firebase.auth();
+export default {
+  data() {
+    return {
+      // Framework7 Parameters
+      f7params: {
+        name: "NYSL", // App name
+        theme: "auto", // Automatic theme detection
+
+        // App routes
+        routes: routes,
+        // Register service worker
+        serviceWorker: {
+          path: "/service-worker.js"
+        },
+        auth: firebase.auth(),
+        isSignedIn: false
+      },
+
+      // Login screen data
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      // this.$f7.dialog.alert(
+      //   "Username: " + this.username + "<br>Password: " + this.password
+      // );
+      auth
+        .signInWithEmailAndPassword(this.username, this.password)
+        .catch(error => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+      console.log(this.username + " " + this.password);
+    }
+  },
+  mounted() {
+    this.$f7ready(f7 => {
+      // Call F7 APIs here
+      auth.onAuthStateChanged(user => {
+        if (user) {
+          // User is signed in.
+          this.isSignedIn = true;
+          console.log(user);
+        } else {
+          // User is signed out.
+          // ...
+        }
+      });
+    });
+  }
+};
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,93 @@
+<template>
+  <f7-page name="home">
+    <!-- Top Navbar -->
+    <f7-navbar :sliding="false" large>
+      <f7-nav-left>
+        <f7-link icon-ios="f7:menu" icon-aurora="f7:menu" icon-md="material:menu" panel-open="left"></f7-link>
+      </f7-nav-left>
+      <f7-nav-title sliding>NYSL</f7-nav-title>
+      
+      <f7-nav-title-large sliding>NYSL</f7-nav-title-large>
+      <f7-nav-right>
+        <f7-link icon-ios="f7:contact" icon-aurora="f7:person" icon-md="material:person" fill raised login-screen-open="#login-screen">Log-In</f7-link>
+      </f7-nav-right>
+
+    </f7-navbar>
+
+    <!-- Page content-->
+    <f7-block strong>
+      <p>This is an example of tabs-layout application. The main point of such tabbed layout is that each tab contains independent view with its own routing and navigation.</p>
+
+      <p>Each tab/view may have different layout, different navbar type (dynamic, fixed or static) or without navbar like this tab.</p>
+    </f7-block>
+
+    <f7-block-title>Navigation</f7-block-title>
+    <f7-list>
+      <f7-list-item link="/about/" title="About"></f7-list-item>
+      <f7-list-item link="/form/" title="Form"></f7-list-item>
+    </f7-list>
+
+<div>
+  {{ teams[0].players[0].first_name }}
+</div>
+
+    <!-- <f7-block-title>Modals</f7-block-title>
+    <f7-block strong>
+      <f7-row>
+        <f7-col width="50">
+          <f7-button fill raised popup-open="#my-popup">Popup</f7-button>
+        </f7-col>
+        <f7-col width="50">
+          <f7-button fill raised login-screen-open="#my-login-screen">Login Screen</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+
+    <f7-block-title>Panels</f7-block-title>
+    <f7-block strong>
+      <f7-row>
+        <f7-col width="50">
+          <f7-button fill raised panel-open="left">Left Panel</f7-button>
+        </f7-col>
+        <f7-col width="50">
+          <f7-button fill raised panel-open="right">Right Panel</f7-button>
+        </f7-col>
+      </f7-row>
+    </f7-block>
+
+    <f7-list>
+      <f7-list-item
+        title="Dynamic (Component) Route"
+        link="/dynamic-route/blog/45/post/125/?foo=bar#about"
+      ></f7-list-item>
+      <f7-list-item title="Default Route (404)" link="/load-something-that-doesnt-exist/"></f7-list-item>
+      <f7-list-item title="Request Data & Load" link="/request-and-load/user/123456/"></f7-list-item>
+    </f7-list> -->
+  </f7-page>
+</template>
+<script>
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/database");
+export default {
+  data() {
+    return {
+      teams: {}
+    };
+  },
+  mounted() {
+    // Your web app's Firebase configuration
+
+    const db = firebase.database();
+    db.ref("/teams/")
+      .once("value")
+      .then(snapshot => {
+        this.teams = snapshot.val();
+        console.log(this.teams)
+      });
+  }
+};
+</script>
